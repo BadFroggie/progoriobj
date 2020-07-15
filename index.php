1a29f730bc3b673fa0ce8a2f21c270ee983011a7
BadFroggie <68106114+BadFroggie@users.noreply.github.com>
2020-07-15 16:15:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-git status
+<?php
+
+require('src/Character.php');
+require('src/Archer.php');
+
+$hero = new Character("Gandalf", "Vous ne passerez paaaaaaas !!!");
+$villain = new Character("Ouroukaï", "Grrrrrrrraaaarkrrrrr !");
+$elfe = new Archer("Legolas", "Pour Arwen !", 25);
+echo $hero->name;?> : 
+<?php echo $hero->getWarcry() . "<br>";
+echo $villain->name; ?> : 
+<?php echo $villain->getWarcry() . "<br>";
+echo $elfe->name; ?> : 
+<?php echo $elfe->getWarcry() . "<br><br>";
+
+echo $villain->hp . "<br>";
+$elfe->rangeAttack($villain);
+
+echo $villain->hp . "<br>";
